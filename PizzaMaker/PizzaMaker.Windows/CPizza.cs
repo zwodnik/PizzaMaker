5fa8d08a01f4c339f0f1b8a55e38ce5edf55f17d
kroQ <morvidkrk@gmail.com>
2014-12-12 20:10:28 +0100

    Dodanie Pizzy v1.0

---
+++ PizzaMaker/PizzaMaker.Windows/CPizza.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaMaker
+{
+    class CPizza
+    {
+        private int Podstawa; // Cena podstawowa pizzy
+        private double Mnoznik; //pizzy
+        private int calkowity; //koszt pizzy
+        public CPizza()
+        {
+            this.Podstawa = 15;
+            this.Mnoznik = 1;
+        }
+
+        public int calkowity(int suma_skladnikow, int podstawa)
+        {
+            int suma = (int)((suma_skladnikow + podstawa) * Mnoznik);
+            return suma;
+        }
+
+        public enum ENUMMnoznik()
+    {
+
+    }
+    }
+}
